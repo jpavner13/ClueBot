f9adf24ee796969274db074a60b0ff88369d0e61
citren0 <cameron.davis1944@gmail.com>
2024-04-14 22:07:21 -0600

    Added adjacent moves function for players. Added PlayerTest for the adjacent moves function.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Bot {
     private final String botName;
@@ -29,5 +30,49 @@ public class Bot {
 
     public ArrayList<Card> getHand(){
         return this.cardsInHand;
+    }
+
+    public List<int[]> getAdjacentMoves()
+    {
+        int[] currPosition = this.getBoardPosition();
+
+        GameBoard board = GameBoard.getInstance();
+        String[][] boardArray = board.getBoard();
+
+        int boardWidth = boardArray[0].length;
+        int boardHeight = boardArray.length;
+
+        // At most there can be 4 possible moves.
+        List<int[]> possibleMoves = new ArrayList<>();
+
+        // Move left / west.
+        if ( (currPosition[0] > 0) && (boardArray[currPosition[0] - 1][currPosition[1]].equals("Empty")) )
+        {
+            int[] move = {currPosition[0] - 1, currPosition[1]};
+            possibleMoves.add(move);
+        }
+
+        // Move up / north.
+        if ( (currPosition[1] > 0) && (boardArray[currPosition[0]][currPosition[1] - 1].equals("Empty")) )
+        {
+            int[] move = {currPosition[0], currPosition[1] - 1};
+            possibleMoves.add(move);
+        }
+
+        // Move right / east.
+        if ( (currPosition[0] < boardWidth - 1) && (boardArray[currPosition[0] + 1][currPosition[1]].equals("Empty")) )
+        {
+            int[] move = {currPosition[0] + 1, currPosition[1]};
+            possibleMoves.add(move);
+        }
+
+        // Move down / south.
+        if ( (currPosition[1] < boardHeight - 1) && (boardArray[currPosition[0]][currPosition[1] + 1].equals("Empty")) )
+        {
+            int[] move = {currPosition[0], currPosition[1] + 1};
+            possibleMoves.add(move);
+        }
+
+        return possibleMoves;
     }
 }
