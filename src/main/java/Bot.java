4198db2a2ad8f7b2f3c35f6b326b41a09baeed33
Brian Avner <brianavner@gmail.com>
2024-04-14 21:07:36 -0600

    Minor changes, realized I was on main, oops

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
+
 public class Bot {
-    public void addCardToHand(Card remove) {
-        //Todo add this implementation
+    private final String botName;
+    private int[] boardPosition;
+    private final ArrayList<Card> cardsInHand;
+
+    public Bot (String name, int[] startingBoardPosition){
+        this.botName = name;
+        this.boardPosition = startingBoardPosition;
+        cardsInHand = new ArrayList<Card>();
     }
-    // Abstract bot class, will define functionality needed for a bot turn cycle.
+
+    public String getPlayerName(){
+        return this.botName;
+    }
+
+    public int[] getBoardPosition(){
+        return this.boardPosition;
+    }
+
+    public void setBoardPosition(int[] newBoardPosition){
+        this.boardPosition = newBoardPosition;
+    }
+
+    public void addCardToHand(Card newCard){
+        this.cardsInHand.add(newCard);
+    }
+
+    public ArrayList<Card> getHand(){
+        return this.cardsInHand;
+    }
 }
