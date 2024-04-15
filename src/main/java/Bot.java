af1801dc17de8c42b34804d0bfa00f0e2db5dd1b
citren0 <cameron.davis1944@gmail.com>
2024-04-14 22:08:14 -0600

    Added same adjacent moves function to the bot.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -46,33 +46,43 @@ public class Bot {
         List<int[]> possibleMoves = new ArrayList<>();
 
         // Move left / west.
-        if ( (currPosition[0] > 0) && (boardArray[currPosition[0] - 1][currPosition[1]].equals("Empty")) )
-        {
-            int[] move = {currPosition[0] - 1, currPosition[1]};
-            possibleMoves.add(move);
-        }
-
-        // Move up / north.
-        if ( (currPosition[1] > 0) && (boardArray[currPosition[0]][currPosition[1] - 1].equals("Empty")) )
+        // Player can only go to a position if it is either a door or empty.
+        if ( (currPosition[1] > 0) &&
+                ((boardArray[currPosition[0]][currPosition[1] - 1].equals("Empty")) ||
+                        boardArray[currPosition[0]][currPosition[1] - 1].contains("Door")) )
         {
             int[] move = {currPosition[0], currPosition[1] - 1};
             possibleMoves.add(move);
         }
 
-        // Move right / east.
-        if ( (currPosition[0] < boardWidth - 1) && (boardArray[currPosition[0] + 1][currPosition[1]].equals("Empty")) )
+        // Move up / north.
+        if ( (currPosition[0] > 0) &&
+                (boardArray[currPosition[0] - 1][currPosition[1]].equals("Empty") ||
+                        boardArray[currPosition[0] - 1][currPosition[1]].contains("Door")) )
         {
-            int[] move = {currPosition[0] + 1, currPosition[1]};
+            int[] move = {currPosition[0] - 1, currPosition[1]};
             possibleMoves.add(move);
         }
 
-        // Move down / south.
-        if ( (currPosition[1] < boardHeight - 1) && (boardArray[currPosition[0]][currPosition[1] + 1].equals("Empty")) )
+        // Move right / east.
+        if ( (currPosition[1] < boardWidth - 1) &&
+                (boardArray[currPosition[0]][currPosition[1] + 1].equals("Empty") ||
+                        boardArray[currPosition[0]][currPosition[1] + 1].contains("Door")) )
         {
             int[] move = {currPosition[0], currPosition[1] + 1};
             possibleMoves.add(move);
         }
 
+        // Move down / south.
+        if ( (currPosition[0] < boardHeight - 1) &&
+                (boardArray[currPosition[0] + 1][currPosition[1]].equals("Empty") ||
+                        boardArray[currPosition[0] + 1][currPosition[1]].contains("Door")) )
+        {
+            int[] move = {currPosition[0] + 1, currPosition[1]};
+            possibleMoves.add(move);
+        }
+
         return possibleMoves;
     }
+    
 }
