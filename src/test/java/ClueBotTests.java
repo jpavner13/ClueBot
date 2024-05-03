f262de1b485b6a801fd5a77db1a28dcb45e05175
Brian Avner <brianavner@gmail.com>
2024-05-03 14:14:37 -0600

    Added test to ensure bot will move to pool and make final guess in actual game

--- src/test/java/ClueBotTests.java
+++ src/test/java/ClueBotTests.java
@@ -23,6 +23,17 @@ public class ClueBotTests extends javax.swing.JFrame{
         ArrayList<Card> poolCards = solutionAndExtraCards.get(1);
         ArrayList<Card> allCards = solutionAndExtraCards.get(2);
 
+        //Find the "pool" card
+        int indexOfPoolCard = 0;
+        for (int i = 0; i < allCards.size(); i++) {
+            if (allCards.get(i).getCardName().equals("Pool")) {
+                indexOfPoolCard = i;
+            }
+        }
+        Card poolCard = allCards.get(indexOfPoolCard);
+
+        bot.storePoolCard(poolCard);
+
         DisplayWindowPage display = new DisplayWindowPage(bot, allCards, players);
 
         GameplayActions.seeOtherPlayersAndPossibleCards(players, bots, allCards);
@@ -54,5 +65,59 @@ public class ClueBotTests extends javax.swing.JFrame{
         players.add(white);
         players.add(scarlet);
         return players;
+    }
+
+    // Bot should go to the pool and make a final guess
+    @Test
+    public void testClueBotSolutionKnown() throws InterruptedException, MalformedURLException, PropertyVetoException {
+        ArrayList<Bot> bots = new ArrayList<Bot>();
+
+        String botName = "Mustard";
+        int[] startingPos = {28, 7};
+        Bot bot = new Bot(botName, startingPos, new BotAllAvailableInfoDeductionStrategy());
+
+        bots.add(bot);
+
+        ArrayList<Player> players = getPlayers();
+
+        ArrayList<ArrayList<Card>> solutionAndExtraCards = GameplayActions.dealCards(players, bots);
+
+        ArrayList<Card> mysterySolution = solutionAndExtraCards.get(0);
+        ArrayList<Card> poolCards = solutionAndExtraCards.get(1);
+        ArrayList<Card> allCards = solutionAndExtraCards.get(2);
+
+        ArrayList<Card> cardsNotInSolution = new ArrayList<>(allCards);
+        cardsNotInSolution.removeAll(mysterySolution);
+
+        // Ensure bot knows the solution
+        for (Card card : cardsNotInSolution) {
+
+            if (!bot.cardsDeducedNotSolution.contains(card)){
+                bot.cardsDeducedNotSolution.add(card);
+            }
+        }
+
+        //Find the "pool" card
+        int indexOfPoolCard = 0;
+        for (int i = 0; i < allCards.size(); i++) {
+            if (allCards.get(i).getCardName().equals("Pool")) {
+                indexOfPoolCard = i;
+            }
+        }
+        Card poolCard = allCards.get(indexOfPoolCard);
+
+        bot.storePoolCard(poolCard);
+
+        DisplayWindowPage display = new DisplayWindowPage(bot, allCards, players);
+
+        GameplayActions.seeOtherPlayersAndPossibleCards(players, bots, allCards);
+
+        for(Card card : bot.getHand()){
+            display.addCardToHand(card.getCardName());
+        }
+
+        while (display.frame.isVisible()) {
+            Thread.sleep(100); // Sleep for a short interval to avoid CPU usage
+        }
     }
 }
